4577618b9cc4a6f3a46f67ebec5a60ed785bcaec
alxjnsk005 <alxjnsk005@gmail.com>
2025-04-07 16:51:23 -0400

    04/07/25
    Identifying Root Causes Activity

---
+++ story_content/6RTKOV9SV3b_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.49.24347.0%0D%0A%0D%0A00:00:00.096%20--%3E%2000:00:03.374%0D%0AFollow%20the%20on-screen%20instructions%20to%20complete%20this%20activity.%0D%0A%0D%0A"}')
